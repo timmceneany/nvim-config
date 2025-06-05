04933c4f2f18bc371b696ecfbc633be03d9607db
Tim McEneany <tim.mceneany@aquatic.com>
2025-06-05 13:43:26 -0500

    added keymaps and tweaked lualine

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,8 @@
 return {
-	"lewis6991/gitsigns.nvim",
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    local gitsigns = require("gitsigns")
+    vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk)
+    vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk)
+  end,
 }
