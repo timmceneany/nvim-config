7c0848c2c3b9d97a46f6e28c5f36c2a314016fbd
Tim McEneany <tim.mceneany@aquatic.com>
2025-06-11 08:50:27 -0500

    add rust analyzer

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -17,6 +17,7 @@ return {
       local capabilities = require("blink.cmp").get_lsp_capabilities()
       require("lspconfig").lua_ls.setup({ capabilities = capabilities })
       require("lspconfig").pyright.setup({ capabilities = capabilities })
+      require("lspconfig").rust_analyzer.setup({ capabilities = capabilities })
     end,
   },
 }
