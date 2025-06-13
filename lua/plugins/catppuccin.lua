4aca1479fcb632a30c8dbdc933ef9979075ff73e
tmceneany <tim.mceneany@aquatic.com>
2025-06-13 11:37:58 -0500

    changed theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,8 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "catppuccin-macchiato"
-    end
-  }
+      -- vim.cmd.colorscheme "catppuccin-macchiato"
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
 }
