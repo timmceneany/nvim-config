796e6809897132e12a4297cd659aec22cc3c620d
timmceneany <tim.mceneany@hey.com>
2026-07-16 12:33:31 -0500

    added telescope references and hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,13 +15,16 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader><leader>", function()
+        builtin.find_files({ hidden = true })
+      end, {})
       vim.keymap.set("n", "<leader>,", builtin.buffers, { desc = "List buffers" })
       vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
       --vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
       vim.keymap.set("n", "<leader>o", function()
         builtin.lsp_document_symbols({ symbols = { "function", "method", "class" } })
       end, { desc = "Document symbols (functions)" })
+      vim.keymap.set("n", "<leader>r", builtin.lsp_references, { desc = "List references" })
 
       require("telescope").load_extension("ui-select")
     end,
